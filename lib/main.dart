2a67a1e30463887aaaee387de878f10c39876c87
marina Sedki <frshenoudamarina@gmail.com>
2024-01-10 17:20:48 +0200

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:market/features/splash/presentation/widgets/screen12.dart';
 
 import 'features/splash/presentation/paidpage.dart';
-import 'features/splash/presentation/widgets/paidpage.dart';
+import 'features/splash/presentation/widgets/radio.dart';
 import 'features/splash/presentation/widgets/test.dart';
 
 void main() {
@@ -16,7 +16,7 @@ class FruitsMarket extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PaidPage(),
+      home: RadioExampleApp(),
     );
   }
 }
